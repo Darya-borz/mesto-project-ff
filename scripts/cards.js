99de14184d1f658715c2f5e42b6fb017fc974dca
Darya Borzova <daryaborz@mail.ru>
2024-02-06 23:57:27 +0300

    Первая версия скрипта

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Горы"
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Озеро"
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Дом"
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Поле"
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Дорога"
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Скала"
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция удаления карточки
 
+function deleteCard(item) {
+    item.remove();
+    return 0;
+}
+
+// @todo: Функция создания карточки
+
+function creatCard(card, deletCard) {
+    let itemCard = cardTemplate.querySelector('.card').cloneNode(true);
+
+    itemCard.querySelector('.card__image').src = card.link;
+    itemCard.querySelector('.card__title').textContent = card.name;
+
+    const deleteButton = itemCard.querySelector('.card__delete-button');
+
+    deleteButton.addEventListener("click", () => deleteCard(itemCard));
+
+    return itemCard;
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((item) => {
+    placesList.append(creatCard(item));
+});
